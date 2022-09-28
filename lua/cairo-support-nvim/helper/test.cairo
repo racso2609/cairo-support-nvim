25d1a9dbfee9bb0809ecdf6666546249a4823281
racso2609 <oscaremiliolugo@gmail.com>
2022-09-28 17:20:37 -0400

    format cairo autocmd v0.1

---
+++ lua/cairo-support-nvim/helper/test.cairo
@@ -0,0 +1,4 @@
+func hola(a:felt){
+
+  return ()
+}
--- lua/cairo-support-nvim/init.lua
+++ lua/cairo-support-nvim/init.lua
@@ -1,1 +1,15 @@
 print("cairo")
+local autocmd = vim.api.nvim_create_autocmd
+local M = {}
+function M.setup()
+	autocmd("BufWritePost", {
+		pattern = "*.cairo",
+		callback = function()
+			local filePath = vim.fn.expand("%:p")
+			vim.cmd("cp " .. filePath .. " " .. filePath .. ".old")
+			vim.cmd("!cairo-format " .. filePath)
+		end,
+	})
+end
+
+return M
