745bfdac72b75de3870f6a21feb84f0274a735a5
racso2609 <oscaremiliolugo@gmail.com>
2022-10-01 16:28:05 -0400

    add command to active env

--- lua/cairo-support-nvim/init.lua
+++ lua/cairo-support-nvim/init.lua
@@ -2,105 +2,115 @@ local autocmd = vim.api.nvim_create_autocmd
 local utils = require("cairo-support-nvim.utils")
 local FORMAT_COMMAND = "!cairo-format "
 local COMPILE_COMMAND = "!starknet-compile "
+local START_ENV_COMMAND = "!pyenv active nexera_cairo"
 
 function HandleErr(errorPos, errorString)
-	local bnr = 0
-	local ns_id = vim.api.nvim_create_namespace("cairo_erros")
-	local line_num = errorPos[1] - 1
+  local bnr = 0
+  local ns_id = vim.api.nvim_create_namespace("cairo_erros")
+  local line_num = errorPos[1] - 1
 
-	local opts = {
-		id = 1,
-		virt_text = { { errorString, vim.api.nvim_get_hl_id_by_name("error") } },
-	}
+  local opts = {
+    id = 1,
+    virt_text = { { errorString, vim.api.nvim_get_hl_id_by_name("error") } },
+  }
 
-	vim.api.nvim_buf_set_extmark(bnr, ns_id, line_num, -1, opts)
+  vim.api.nvim_buf_set_extmark(bnr, ns_id, line_num, -1, opts)
 end
 
 function GetErrorInfo(data)
-	local errorNumberLine = string.match(data, "%d+[:]%d+")
-	if not errorNumberLine then
-		return
-	end
-	-- print("numberLine", errorNumberLine)
-	local errorSplit = vim.split(errorNumberLine, ":")
-	local errorPos = {}
-	for _, value in ipairs(errorSplit) do
-		table.insert(errorPos, tonumber(value))
-	end
+  local errorNumberLine = string.match(data, "%d+[:]%d+")
+  if not errorNumberLine then
+    return
+  end
+  -- print("numberLine", errorNumberLine)
+  local errorSplit = vim.split(errorNumberLine, ":")
+  local errorPos = {}
+  for _, value in ipairs(errorSplit) do
+    table.insert(errorPos, tonumber(value))
+  end
 
-	local startIndex = string.find(data, "[.]%s")
-	local finishIndex = string.find(data, "[.]\n")
+  local startIndex = string.find(data, "[.]%s")
+  local finishIndex = string.find(data, "[.]\n")
 
-	if not startIndex or not finishIndex then
-		return
-	end
+  if not startIndex or not finishIndex then
+    return
+  end
 
-	local result = {}
-	table.insert(result, errorPos)
-	local errorString = string.sub(data, startIndex + 1, finishIndex - 1)
-	table.insert(result, " " .. errorString)
-	-- print("errorString", errorString)
+  local result = {}
+  table.insert(result, errorPos)
+  local errorString = string.sub(data, startIndex + 1, finishIndex - 1)
+  table.insert(result, " " .. errorString)
+  -- print("errorString", errorString)
 
-	return result
+  return result
 end
 
 function Format()
-	autocmd("BufWritePost", {
-		pattern = "*.cairo",
-		callback = function()
-			local filePath = vim.fn.expand("%:p")
-			local currentPos = vim.api.nvim_win_get_cursor(0)
+  autocmd("BufWritePost", {
+    pattern = "*.cairo",
+    callback = function()
+      local filePath = vim.fn.expand("%:p")
+      local currentPos = vim.api.nvim_win_get_cursor(0)
 
-			local data = vim.api.nvim_exec(FORMAT_COMMAND .. filePath, true)
+      local data = vim.api.nvim_exec(FORMAT_COMMAND .. filePath, true)
 
-			local errorData = GetErrorInfo(data)
+      local errorData = GetErrorInfo(data)
 
-			if errorData then
-				-- HandleErr(errorData[1], errorData[2])
-				return
-			end
-			local actualContent = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+      if errorData then
+        -- HandleErr(errorData[1], errorData[2])
+        return
+      end
+      local actualContent = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
-			local dataArr = vim.split(data, "\n")
+      local dataArr = vim.split(data, "\n")
 
-			table.remove(dataArr, 1)
-			table.remove(dataArr, 1)
+      table.remove(dataArr, 1)
+      table.remove(dataArr, 1)
 
-			local dataIsEqToActualContent = utils.isEqArr(dataArr, actualContent)
-			if not dataIsEqToActualContent then
-				vim.api.nvim_buf_set_lines(0, 0, -1, false, { "" })
-				vim.api.nvim_buf_set_text(0, 0, 0, 0, 0, dataArr)
-				vim.api.nvim_win_set_cursor(0, currentPos)
-				print("formated...")
-			end
-		end,
-	})
+      local dataIsEqToActualContent = utils.isEqArr(dataArr, actualContent)
+      if not dataIsEqToActualContent then
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, { "" })
+        vim.api.nvim_buf_set_text(0, 0, 0, 0, 0, dataArr)
+        vim.api.nvim_win_set_cursor(0, currentPos)
+        print("formated...")
+      end
+    end,
+  })
 end
 
 function Compile()
-	autocmd("BufWritePost", {
-		pattern = "*.cairo",
-		callback = function()
-			local filePath = vim.fn.expand("%:p")
-			print("compiling...")
-			local data = vim.api.nvim_exec(COMPILE_COMMAND .. filePath, true)
-			local errorData = GetErrorInfo(data)
-			if errorData then
-				HandleErr(errorData[1], errorData[2])
-				return
-			end
-		end,
-	})
+  autocmd("BufWritePost", {
+    pattern = "*.cairo",
+    callback = function()
+      local filePath = vim.fn.expand("%:p")
+      print("compiling...")
+      local data = vim.api.nvim_exec(COMPILE_COMMAND .. filePath, true)
+      local errorData = GetErrorInfo(data)
+      if errorData then
+        HandleErr(errorData[1], errorData[2])
+        return
+      end
+    end,
+  })
 end
 
 local M = {}
 function M.setup(config)
-	if config.format then
-		Format()
-	end
-	if config.compile then
-		Compile()
-	end
+  if config.format then
+    Format()
+  end
+  if config.compile then
+    Compile()
+  end
+
+  autocmd("FileType", {
+    pattern = "*.cairo",
+    callback = function()
+      print("cairo")
+
+      vim.api.nvim_exec(START_ENV_COMMAND, false)
+    end,
+  })
 end
 
 -- Collect all key-value pairs from the given string into a table
